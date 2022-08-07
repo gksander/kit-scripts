859c273fa8d1feea3941e2530d5e456e471f7228
Grant Sander <gksander93@gmail.com>
2022-08-07 18:21:17 -0500

    Dark mode

---
+++ toggle-dark-mode.ts
@@ -0,0 +1,18 @@
+// Menu: Toggle Dark Mode
+// Description: Switches osx between light/dark modes
+// Author: John Lindquist
+// Twitter: @johnlindquist
+
+import "@johnlindquist/kit";
+
+await applescript(`
+tell application "System Events"
+
+    tell appearance preferences
+
+        set dark mode to not dark mode
+
+    end tell
+
+end tell
+`);
